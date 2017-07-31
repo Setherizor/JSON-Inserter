c09367eb8f123606013d9470a5e12d7289c40bfb
Setherizor <setherizor@gmail.com>
2017-07-30 21:31:53 -0400

    Removed eval statement & applied standard formatting

--- inserter.js
+++ inserter.js
@@ -1,54 +1,53 @@
-URL = "./data.json";
+const URL = './data.json'
 
 var doRequest = function (callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', URL, true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', URL, true)
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+      callback(xobj.responseText)
+    }
+  }
+  xobj.send(null)
 }
 var apply = function (value, obj, el) {
-    if (value == "body") {
-        eval("el.innerHTML = obj." + value);
-    } else {
-        if (eval("obj.style." + value) != undefined) {
-            eval("el.style." + value + "= obj.style." + value);
-        }
+  if (value === 'body') {
+    el.innerHTML = obj[value]
+  } else {
+    if (obj.style[value] !== undefined) {
+      el.style[value] = obj.style[value]
     }
+  }
 }
 var handleAttributes = function (obj, el) {
-    if (obj.type == "html") {
-        apply("body", obj, el);
-        for (var i = 0; i < Object.keys(obj.style).length; i++) {
-            var curr = Object.keys(obj.style)[i];
-            apply(curr, obj, el);
-        }
-    } else {
-        el.innerText = obj;
-        return;
+  if (obj.type === 'html') {
+    apply('body', obj, el)
+    for (var i = 0; i < Object.keys(obj.style).length; i++) {
+      var curr = Object.keys(obj.style)[i]
+      apply(curr, obj, el)
     }
+  } else {
+    el.innerText = obj
+  }
 }
 var applyData = function (json) {
-    var length = Object.keys(json).length;
-    for (var i = 0; i < length; i++) {
-        var currName = Object.keys(json)[i];
-        var elements = document.getElementsByName(currName);
-        if (elements[0] != undefined) {
-            for (var j = 0; j < elements.length; j++) {
-                handleAttributes(json[currName], elements[j])
-            }
-        }
+  var length = Object.keys(json).length
+  for (var i = 0; i < length; i++) {
+    var currName = Object.keys(json)[i]
+    var elements = document.getElementsByName(currName)
+    if (elements[0] !== undefined) {
+      for (var j = 0; j < elements.length; j++) {
+        handleAttributes(json[currName], elements[j])
+      }
     }
+  }
 }
 var getData = function () {
-    doRequest(function (response) {
-        var actual_JSON = JSON.parse(response);
-        applyData(actual_JSON);
-    });
+  doRequest(function (response) {
+    var actual = JSON.parse(response)
+    applyData(actual)
+  })
 }
-getData();
+getData()
